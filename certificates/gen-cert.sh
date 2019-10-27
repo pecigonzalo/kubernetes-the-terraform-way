338750357ed84505d79e2cf2af77cc70f5a6287e
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2019-10-27 11:31:13 +0100

    Add certificate generation

---
+++ certificates/gen-cert.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+instance="${1}"
+
+NAMES=$(
+  jq -r \
+    '[.[].value | with_entries(select(.key|test("'"${instance}"'"))) | .[]] | join(",")' \
+    nodes.json
+)
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname="${NAMES}" \
+  -profile=kubernetes \
+  "${instance}"-csr.json | cfssljson -bare "${instance}"
